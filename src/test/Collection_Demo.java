b9f697cb60990d47db0f2045b0e30a519c14e716
zang <1114133631@qq.com>
2020-05-20 16:54:29 +0800

    Merge branch 'ToOffer' of github.com:swjtxhZhou/DayliPractice into ToOffer
    # Conflicts:
    #	.idea/workspace.xml

---
+++ src/test/Collection_Demo.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Collection_Demo {
+    public static void main(String[] args){
+        Map<Integer,String> map = new HashMap<>();
+        for(String val:map.values()){
+
+        }
+        for(Integer integer:map.keySet()){
+
+        }
+
+    }
+}
---
+++ src/test/Interface_Demo.java
@@ -0,0 +1,7 @@
+package test;
+
+interface Interface_Demo {
+    Integer i=0;//必须要赋初值，默认是static和final的
+
+
+}
